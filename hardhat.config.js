0055ecabb0c0da9e09eb931138d7a497bb24095b
halfdoctor <nmanura@gmail.com>
2025-03-29 10:01:04 +0000

    CHANGE: powerloom-mainnet-v2 chain

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,19 +2,6 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "dotenv/config";
 
-// import "@nomicfoundation/hardhat-chai-matchers";
-// import "@nomicfoundation/hardhat-ethers";
-// import "hardhat-gas-reporter";
-
-// export default {
-//   solidity: "0.8.24",
-//   networks: {
-//     hardhat: {
-//       chainId: 31337,  // Default chain ID for local Hardhat network
-//     },
-//   },
-// };
-
 export default {
   solidity: {
     version: "0.8.24",
@@ -42,15 +29,15 @@ export default {
   },
   etherscan: {
     apiKey: {
-      'powerloom-mainnet': 'empty'
+      'powerloom-mainnet-v2': 'empty'
     },
     customChains: [
       {
-        network: "powerloom-mainnet",
-        chainId: 7865,
+        network: "powerloom-mainnet-v2",
+        chainId: 7869,
         urls: {
-          apiURL: "https://explorer-powerloom-mainnet-hdsv5hx40a.t.conduit.xyz/api",
-          browserURL: "https://explorer-powerloom-mainnet-hdsv5hx40a.t.conduit.xyz:443"
+          apiURL: "https://explorer-powerloom-mainnet-v2-v52tbqo4if.t.conduit.xyz/api",
+          browserURL: "https://explorer-powerloom-mainnet-v2-v52tbqo4if.t.conduit.xyz:443"
         }
       }
     ]
